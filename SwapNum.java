82da17a5d003000cf80655a294b70de4a02480c8
Keerthana KJ <87189032+keerthanakj@users.noreply.github.com>
2023-06-18 17:19:55 +0530

    Add files via upload

---
+++ SwapNum.java
@@ -0,0 +1,19 @@
+package mySwap;
+
+public class SwapNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =30;
+		int b =10;
+		int temp;
+		
+		temp = a;
+		a = b;
+		b = temp;
+		
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+	}
+
+}
